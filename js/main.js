0bcac9572c3d09b22e4b5c8afb7f6f0a5b78bede
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2024-10-04 23:58:04 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const navBtn = document.querySelector('.nav-icon-btn')
+
+const navIcon = document.querySelector('.nav-icon')
+
+const nav = document.querySelector('.header__top-row')
+
+
+
+
+navBtn.onclick = function(){
+    navIcon.classList.toggle('nav-icon--active')
+    nav.classList.toggle('header__top-row--mobile')
+    document.body.toggle('.no-scroll')
+}
